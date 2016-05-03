ca6d19f31dcbf8951584c94a5e7422d5b6a086a6
Gilberto11 <surfcorinthians@hotmail.com>
2016-05-03 13:13:25 +0100

    initial commit

---
+++ Day3Example5Main.java
@@ -0,0 +1,16 @@
+
+public class Day3Example5Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3, b=2, c=1, d=0;
+		int result1= a++ + --b + c++ + --d - --c - b-- + ++a + a++;
+		System.out.println("a is: " + a);
+		System.out.println("b is: " + b);
+		System.out.println("c is: " + c);
+		System.out.println("d is: " + d);
+		
+
+	}
+
+}
